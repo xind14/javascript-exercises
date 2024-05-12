a07d6ff4af83580347e86f54aad32e971c3cbd7f
Xin Deng <xind1470@gmail.com>
2024-05-12 01:50:17 -0700

    all test passing for repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let str = '';
+  if (num<0) return "ERROR";
+  for (let i=0; i<num; i++){
+    str+=string
+  }
+  return str;
 
 };
 
